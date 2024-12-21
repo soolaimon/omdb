40cc90b6c777536ce51d6ce40de62ed002814f04
David Nelson <davidicus21@gmail.com>
2024-12-21 15:12:29 -0700

    package

--- mix.exs
+++ mix.exs
@@ -7,7 +7,10 @@ defmodule Omdb.MixProject do
       version: "0.1.0",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: "A Thin Wrapper for the OMDB API",
+      package: package(),
+      source_url: "https://github.com/soolaimon/omdb"
     ]
   end
 
@@ -25,6 +28,16 @@ defmodule Omdb.MixProject do
       {:finch, "~> 0.19"},
       {:mix_test_watch, "1.2.0", only: [:dev, :test]},
       {:mimic, "~> 1.10", only: :test}
+    ]
+  end
+
+  defp package() do
+    [
+      name: "omdb",
+      files: ["lib", "mix.exs", "README.md"],
+      maintainers: ["Dave Nelson"],
+      licenses: ["APACHE"],
+      links: %{"OMDB" => "https://www.omdbapi.com/"}
     ]
   end
 end
