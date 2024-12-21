db71b46d15ee9c7612b1f3f1a46700acbe8bde38
David Nelson <davidicus21@gmail.com>
2024-12-21 12:28:01 -0700

    dbg

--- lib/omdb/client.ex
+++ lib/omdb/client.ex
@@ -2,6 +2,7 @@ defmodule Omdb.Client do
   @base_url "www.omdbapi.com/"
   @image_url "img.omdbapi.com/"
 
+  @spec search(String.t(), keyword()) :: {:ok, map()} | {:error, String.t()}
   def search(query, opts \\ []) do
     res =
       opts
@@ -13,7 +14,7 @@ defmodule Omdb.Client do
       end)
 
     with {:ok, %Finch.Response{status: 200, body: body}} <- res do
-      JSON.decode(body) |> dbg()
+      JSON.decode(body)
     else
       {:ok, %Finch.Response{status: status, body: body}} ->
         {:error, "HTTP Error: #{status} - #{body}"}
